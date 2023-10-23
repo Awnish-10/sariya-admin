d8152998c5d4ec653ad877778ecb67a780236cee
awnishnegi <awnish.negi@okoders.com>
2023-10-23 13:41:41 +0530

    test

--- src/components/ui/Textinput.jsx
+++ src/components/ui/Textinput.jsx
@@ -67,6 +67,7 @@ const Textinput = ({
             disabled={disabled}
             id={id}
             // name={inputName ?? ''}
+            
             onChange={onChange}
           />
         )}
